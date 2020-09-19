a215559a953af45af491d1f4cf6efded2beb775a
eunji lee <eunjilee@eunjiui-MacBook-Pro-93.local>
2020-09-19 21:51:22 +0900

    todolist#2

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 javascript oop lecture sample codes
 
 이화여대 멋쟁이사자처럼 자바스크립트 2주차 수업에서 사용한 예시 코드입니다!
+
+OOP - 자바스크립트 객체 설명 예제
+ToDoList - Todolist 실습 예제
--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -1,14 +1,51 @@
 const toDoForm = document.querySelector(".toDoForm-js"),
     toDoInput = toDoForm.querySelector("input"),
-    toDoList = toDoList.querySelector("toDoList-js");
+    toDoList = document.querySelector(".toDoList-js");
 
+const TODO_LS = "toDos";
+
+//로멀에 저장할 todo list
+const toDos = [];
+
+function paintTodo(text){
+    //console.log(text);
+    //html에 li 생성
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = "❌";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+
+    const toDo_object ={
+        text : text ,
+        id : toDos.length + 1
+    };
+
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintTodo(currentValue);
+    //enter 눌러서 입력하면 창에서 값이 사라지게
+    toDoInput.value = "";
+}
 
 function loadToDos(){
-    const toDos = localStorage.getItem();    
+    const toDos = localStorage.getItem(TODO_LS);
+    if( toDos !== null){
+    }else{
+    //to do 가 없을떄 - 처음
+    }    
 }
 
 function init(){
     loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
+
