afd1c2685fcde6c4321f39ed83913c91593d0c2a
eunji lee <eunjilee@eunjiui-MacBook-Pro-93.local>
2020-09-21 15:14:53 +0900

    delete welcome.js add name.js

--- OOP/oop1.js
+++ OOP/oop1.js
@@ -1,23 +1,23 @@
 //object literal
 
 const book1 = {
-    title : 'book one',
-    author : 'john doe',
-    year : '2013',
+    title : '우리가 빛의 속도로 갈 수 없다면',
+    author : '김초엽',
+    year : '2019',
     getSummary : function(){
-        return `${this.title} was written by ${this.author} in ${this.year}`;
+        return `<${this.title}>은 ${this.author} 작가의 ${this.year}년도 작품이다.`;
     }
 }
 
 const book2 = {
-    title : 'book two',
-    author : 'john doe',
-    year : '2013',
+    title : '해리포터',
+    author : 'jk rowling',
+    year : '1997',
     getSummary : function(){
-        return `${this.title} was written by ${this.author} in ${this.year}`;
+        return `<${this.title}>은 ${this.author} 작가의 ${this.year}년도 작품이다.`;
     }
 }
-
+console.log(book1.title);
 console.log(book1.getSummary());
 console.log(Object.keys(book1));
 console.log(Object.values(book1));
